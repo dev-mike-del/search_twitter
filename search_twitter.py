968ce59ac889cf42536370a0cd05b5ec1e990b8c
root <delgado.portland@gmail.com>
2018-10-26 15:46:56 -0700

    Remove verification code form search_twitter.py

--- search_twitter.py
+++ search_twitter.py
@@ -1,23 +1,8 @@
 import json
 import time
-import urllib3
 import requests
 
-from requests_oauthlib import OAuth1
-
-import keys_and_tokens
-
-consumer_key = keys_and_tokens.consumer_key
-consumer_secret = keys_and_tokens.consumer_secret
-access_token = keys_and_tokens.access_token
-access_token_secret = keys_and_tokens.access_token_secret
-
-verify_credentials_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
-
-auth = OAuth1(consumer_key, consumer_secret,
-	access_token, access_token_secret)
-
-requests.get(verify_credentials_url, auth=auth)
+from twitter_api_verification import auth
 
 screen_name = "realDonaldTrump"
 
