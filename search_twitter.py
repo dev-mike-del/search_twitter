dbff598746bd2179b6e5d5e3c91a8b8447329fb8
i <delgado.portland@gmail.com>
2018-10-26 17:45:28 -0700

    Update search_twitter_profile function to call on search_twitter

--- search_twitter.py
+++ search_twitter.py
@@ -3,4 +3,6 @@
 from search_twitter_profile import search_twitter_profile
 
 if __name__ == '__main__':
+
+	#add a Twitter user's handle as the function parameter
 	search_twitter_profile()
--- search_twitter_profile.py
+++ search_twitter_profile.py
@@ -6,37 +6,22 @@ import requests
 
 from twitter_api_verification import auth
 
-screen_name = "realDonaldTrump"
+def search_twitter_profile(twitter_handle=False):
+	if twitter_handle is not False:
+		search_url = (
+			'https://api.twitter.com/1.1/users/show.json?screen_name={}'.format(
+				twitter_handle
+				)
+			)
 
-search_url = (
-	'https://api.twitter.com/1.1/users/show.json?screen_name={}'.format(screen_name)
-	)
+		results = requests.get(search_url, auth=auth).json()
 
-results = requests.get(search_url, auth=auth).json()
-
-target_keys = ["followers_count"]
-
-def print_key_options(results):
-	for x in results:
-		print(x)
-
-def print_results(results):
-	for x in results:
-		print("{}: {} \n".format(x, results[x]))
-
-def print_target_results(results):
-	for x in results:
-		if x in target_keys:
-			print("{}: {} \n".format(x, results[x]))
-
-def search_twitter_profile():
-	print_key_options(results)
-	print_results(results)
-	print_target_results(results)
+		for result in results:
+			print("{}: {} \n".format(result, results[result]))
+	else:
+		pass
 
 
 if __name__ == "__main__":
-	print_key_options(results)
-	print_results(results)
-	print_target_results(results)
+	search_twitter_profile("dev_mike_del")
 
