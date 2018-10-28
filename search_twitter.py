cb3a9cd6bc831adfebafadc4030d0c31f8ebda38
i <delgado.portland@gmail.com>
2018-10-28 16:10:40 -0700

    Convert search input in search_twitter to int

--- search_twitter.py
+++ search_twitter.py
@@ -7,31 +7,32 @@ from search_twitter_profile_timeline import search_twitter_profile_timeline
 
 if __name__ == '__main__':
 
-print('''
+	search = 0
+
+	print('''
 Welcome to the Python Twitter Search app created by Michael Delgado!
 ''')
 
-search = 0
-
-while search != 4:
-	search = int(
-		input('''
+	while search != 4:
+		search = int(
+			input('''
 1 = Search Twitter Profile
 2 = Search Twitter Profile Followers
 3 = Search Twitter Profile Timeline
 4 = Exit Program
-''')
-		)
 
-	if search == 1:
-		search_twitter_profile(input("Twitter handle: "))
-	elif search == 2:
-		search_twitter_profile_followers(input("Twitter handle: "))
-	elif search == 3:
-		search_twitter_profile_timeline(input("Twitter handle: "))
-	elif search == 4 or search.lower() == "exit":
-		break
-	else:
-		print('''
+''')
+			)
+
+		if search == 1:
+			search_twitter_profile(input("\nTwitter handle: "))
+		elif search == 2:
+			search_twitter_profile_followers(input("\nTwitter handle: "))
+		elif search == 3:
+			search_twitter_profile_timeline(input("\nTwitter handle: "))
+		elif search == 4:
+			break
+		else:
+			print('''
 Please enter the number corresponding to the search that you wish to perform
 ''')
