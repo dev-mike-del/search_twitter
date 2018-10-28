568114c723f75c4b08b22082b897c302f2379144
i <delgado.portland@gmail.com>
2018-10-28 16:03:02 -0700

    Add terminal input to run functions

--- search_twitter.py
+++ search_twitter.py
@@ -2,9 +2,36 @@
 
 from search_twitter_profile import search_twitter_profile
 from search_twitter_profile_followers import search_twitter_profile_followers
+from search_twitter_profile_timeline import search_twitter_profile_timeline
+
 
 if __name__ == '__main__':
 
-	#add a Twitter user's handle as the function parameter
-	search_twitter_profile()
-	search_twitter_profile_followers()
+print('''
+Welcome to the Python Twitter Search app created by Michael Delgado!
+''')
+
+search = 0
+
+while search != 4:
+	search = int(
+		input('''
+1 = Search Twitter Profile
+2 = Search Twitter Profile Followers
+3 = Search Twitter Profile Timeline
+4 = Exit Program
+''')
+		)
+
+	if search == 1:
+		search_twitter_profile(input("Twitter handle: "))
+	elif search == 2:
+		search_twitter_profile_followers(input("Twitter handle: "))
+	elif search == 3:
+		search_twitter_profile_timeline(input("Twitter handle: "))
+	elif search == 4 or search.lower() == "exit":
+		break
+	else:
+		print('''
+Please enter the number corresponding to the search that you wish to perform
+''')
