4bdffd6446a94f75074f9f8c0929b490f8e7890c
i <delgado.portland@gmail.com>
2018-11-06 13:47:49 -0800

    Add search_twitter_profile_followers_ui.py and move search_twitter_profile_followers function and print statements to it

--- search_twitter.py
+++ search_twitter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-
 import datetime
 
 from search_twitter_profile_ui import search_twitter_profile_ui
-from search_twitter_profile_followers import search_twitter_profile_followers
+from search_twitter_profile_followers_ui import search_twitter_profile_followers_ui
 from search_twitter_profile_timeline import search_twitter_profile_timeline
 
 
@@ -47,42 +46,7 @@ Welcome to the Python Twitter Search app created by Michael Delgado!
 				search_twitter_profile_ui(twitter_handle)
 
 			elif search == 2:
-				followers_list = search_twitter_profile_followers(twitter_handle)
-				follower_dict = {}
-				verified_followers = []
-				for follower in followers_list:
-					follower_dict[follower["id_str"]] = {
-													"name":follower["name"],
-													"screen_name":follower["screen_name"],
-													"verified":follower["verified"],
-													"created_at":follower["created_at"],
-													"favourites_count":follower["favourites_count"],
-													"{} is following".format(twitter_handle):follower["following"],
-													"friends_count":follower["friends_count"],
-													"statuses_count":follower["statuses_count"],
-													"description":follower["description"],
-													"followers_count":follower["followers_count"],
-													"lang":follower["lang"],
-													"timestamp": datetime.datetime.now(),
-													}
-					try:
-						follower_dict[follower["id_str"]].update({"url":follower["entities"]["url"]["urls"][0]["expanded_url"]})
-					except KeyError:
-						follower_dict[follower["id_str"]].update({"url":None})
-
-					if follower["verified"] == True:
-						verified_followers.append("{} (@{})".format(follower["name"],follower["screen_name"]))
-
-				print('''
-{} followers.
-
-{} verifired followers
-'''.format(len(followers_list),len(verified_followers)))
-
-				if len(verified_followers) >= 1:
-					print("Verified Followers:")
-					for verified_follower in verified_followers:
-						print(verified_follower)
+				search_twitter_profile_followers_ui(twitter_handle)
 
 			elif search == 3:
 				timeline = search_twitter_profile_timeline(twitter_handle)
